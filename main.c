f3b82e1ffa948d6a01cda8810891c0925a790e1e
Carlos Augusto <capv2004@gmail.com>
2023-11-19 12:44:37 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,209 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int getAmountOfClients(FILE *fp) {
+    int count = 0;
+    char *line = NULL;
+    size_t line_length = 0;
+
+    if (fp == NULL) {
+        perror("Error with file pointer");
+        return -1; 
+    }
+
+    while (getline(&line, &line_length, fp) != -1) {
+        count++;
+    }
+     
+
+    return count;
+}
+
+void initMatrix(int **matrix, int row, int col) {
+    int i, j;
+    
+    matrix = (int **)malloc(row * sizeof(int *));
+    for (i = 0; i < row; i++)
+        matrix[i] = (int *)malloc(col * sizeof(int));
+    for (i = 0; i < row; i++)
+        for (j = 0; j < col; j++)
+            matrix[i][j] = 0;
+}
+
+//change later
+void subtractRows(int i, int matrix1[][i], int need[][i], int rowToSubtract, int rowToSubtractFrom) {
+    for (int j = 0; j < i; j++) {
+        need[rowToSubtractFrom][j] = matrix1[rowToSubtractFrom][j] - matrix1[rowToSubtract][j];
+    }
+}
+
+int inLine(const char *str, const char *substring, size_t substringSize) {
+    size_t substringLength = strlen(substring);
+    if (strncmp(str, substring, substringLength) == 0 && substringLength <= substringSize) {
+        return 1;  
+    } else {
+        return 0;  
+    }
+}
+
+int selectClient(const char *str) {
+    const char *rqPosition = strstr(str, "RQ");
+    const char *rlPosition = strstr(str, "RL");
+
+    if (rqPosition != NULL || rlPosition != NULL) {
+        const char *substring = (rqPosition != NULL) ? rqPosition + 2 : rlPosition + 2;
+
+        int client;
+        if (sscanf(substring, "%d", &client) == 1) {
+            return client;
+        } else {
+            return -1;
+        }
+    }
+}
+
+int **getAllocationRequests(FILE *fp, int totalClients, int totalResources, int amountRequisitions) {
+
+    int **allocationRequests = malloc(totalClients * sizeof(int *));//allocar memoria para o array de requisições
+    for (int i = 0; i < totalClients; i++) {//preencher o array de requisições com base na quantia de clientes
+        allocationRequests[i] = malloc(totalResources * sizeof(int));//agora, criar um array de recursos para cada cliente, com base na quantia dos tipos de recurso.
+    }
+
+    char *line = NULL;
+    size_t line_length = 0;
+
+    if (fp == NULL) {
+        perror("Error with file pointer");
+        return NULL; 
+    }
+
+    for (int i = 0; i < amountRequisitions; i++) {
+        if (getline(&line, &line_length, fp) != -1) { 
+            if (strstr(line, "*") != NULL) {
+                printf("exiting\n");
+
+                    break; // Break the loop if * is found
+            }    
+            char *token = strtok(line, " ");
+            if (token != NULL && (strcmp(token, "RQ") == 0 || strcmp(token, "RL") == 0)) {
+                int client = atoi(strtok(NULL, " "));
+                if (client >= 0 && client < totalClients) {
+                    for (int j = 0; j < totalResources; j++) {
+                        token = strtok(NULL, " ");
+                        if (token != NULL) {
+                            
+                            allocationRequests[client][j] = atoi(token);
+                        } else {
+                            fprintf(stderr, "Error reading from file\n");
+                            return NULL;
+                        }
+                    }
+                } else {
+                    fprintf(stderr, "Invalid client number\n");
+                    return NULL;
+                }
+            } else {
+                fprintf(stderr, "Invalid allocation request\n");
+                return NULL;
+            }
+        } else {
+            fprintf(stderr, "Error reading from file\n");
+            return NULL;
+        }
+    }
+
+    return allocationRequests;
+}
+
+int main(int argc, char *argv[]) {
+    FILE *customers = fopen("customer.txt", "r");
+    FILE *test = fopen("commands.txt", "r");
+    int totalClients = getAmountOfClients(customers);
+    int totalResources = getAmountOfClients(test);
+    printf("%d\n",totalResources);
+    fclose(test);
+    int resourcesPerType[argc - 1];
+    int cliente[totalClients][argc - 1]; 
+    int need[totalClients][argc-1];
+    int **allocation;
+
+        allocation = malloc(totalClients * sizeof(int *));
+    for (int i = 0; i < totalClients; i++) {
+        allocation[i] = malloc((argc - 1) * sizeof(int));
+    }
+
+    for (int i = 0; i < totalClients; i++) {
+        for (int j = 0; j < argc - 1; j++) {
+            allocation[i][j] = 0;
+        }
+    }
+    printf("Allocation Matrix:\n");
+    for (int i = 0; i < totalClients; i++) {
+        for (int j = 0; j < argc - 1; j++) {
+            printf("%d ", allocation[i][j]);
+        }
+        printf("\n");
+    }
+
+
+   
+    for (int i = 1; i < argc; i++) {
+        resourcesPerType[i - 1] = atoi(argv[i]);
+        printf("Total resources of type [%d] = %d\n", i, resourcesPerType[i - 1]);
+    }
+
+   
+    if (customers == NULL) {
+        perror("Fail to read customer.txt\n");
+        return 1;
+    }
+
+    
+    printf("Total clients: %d\n", totalClients);
+
+    rewind(customers);
+
+    for (int i = 0; i < totalClients; i++) {
+        for (int j = 0; j < argc - 1; j++) {
+            if (fscanf(customers, "%d,", &cliente[i][j]) != 1) {
+                fprintf(stderr, "Error reading from file\n");
+                fclose(customers);
+                return 1;
+            }
+        }
+    }
+
+    
+
+    for (int i = 0; i < totalClients; i++) {
+      printf("Cliente %d:\n",i);  
+        for (int j = 0; j < argc - 1; j++) {
+            printf("%d ", cliente[i][j]);
+        }
+        printf("\n");
+    }
+
+   FILE *commands =  fopen ("commands.txt", "r");
+   if (commands == NULL){
+     printf("Fail to read commands.txt");
+     return 1;
+   }
+
+   allocation = getAllocationRequests(commands, totalClients, argc - 1, totalResources);
+   
+   for (int i = 0; i < totalClients; i++) {
+      printf("Requisicao: %d:\n",i);  
+        for (int j = 0; j < argc - 1; j++) {
+            printf("%d ", allocation[i][j]);
+        }
+        printf("\n");
+    }
+
+   fclose(customers);  
+   fclose(commands);
+   
+  
+   FILE *result = fopen("result.txt","w");
+
+    return 0;
+}
